bf5f283f65335a169efa4ac173c6baa0f8cec0b6
adityahh-rm <adityahh.rm@gmail.com>
2021-03-12 09:30:34 +0700

    Menambahkan CSS pada Section History, merubah tampilan Javascript

--- js/parallax.js
+++ js/parallax.js
@@ -7,17 +7,5 @@ window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset; //695
         // console.log(scroll);
     // Parallax Effect
-    if (scroll < 720 ){
-
-    
-        let sectionY = section.getBoundingClientRect();
-            // console.log(SectionY.top); //--> for viewport after scrolling
-        
-        translate.forEach(element => {
-            let speed = element.dataset.speed;
-                //console.log(speed);
-            element.style.transform = `translateY(${scroll * speed}px)`;
-        });
-    }
     
 });
