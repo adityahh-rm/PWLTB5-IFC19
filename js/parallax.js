65b359726b62cd621d5d4db5ba0580036a31b5bf
adityahh-rm <adityahh.rm@gmail.com>
2021-03-12 09:24:10 +0700

    Menambahkan effect Parallax pada halaman Utama

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,23 @@
-// parallax -- HOME
-$(window).scroll(function() {
-    var scrollPos = $(this).scrollTop();
-    $(".hero-back").css({
-          'background-size' : 100 + scrollPos + '%'      
-    });
+const translate = document.querySelectorAll(".translate");
+const navbars = document.querySelector(".navbar");
+const big_title = document.querySelector(".big-title");
+const header = document.querySelector("header");
+
+window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset; //695
+        // console.log(scroll);
+    // Parallax Effect
+    if (scroll < 720 ){
+
+    
+        let sectionY = section.getBoundingClientRect();
+            // console.log(SectionY.top); //--> for viewport after scrolling
+        
+        translate.forEach(element => {
+            let speed = element.dataset.speed;
+                //console.log(speed);
+            element.style.transform = `translateY(${scroll * speed}px)`;
+        });
+    }
+    
 });
